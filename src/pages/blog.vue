21e4357a50092a9711c8139d6e8218baa36a8007
jgarz800 <jgarz800@stu.southtexascollege.edu>
2022-05-09 22:52:56 -0500

    final touches

--- src/pages/blog.vue
+++ src/pages/blog.vue
@@ -1,3 +1,87 @@
 <template>
-    <h1>lol</h1>
+    <div class="flex flex-col items-center justify-center pt-10 space-y-12 min-h-screen-nonav ">
+        <h1 class="px-5 py-2 text-3xl font-semibold text-white bg-red-500 rounded-lg">The Blog</h1>
+    </div>
+    <div class="flex flex-col items-center justify-center pt-10 space-y-12 min-h-screen-nonav ">
+        <h1 class="text-2xl text-red-500">The Giant Pink Cookie Tragedy</h1>
+        <div>
+            <p class="mx-40 text-center text-white ">
+                The long beloved giant pink cookie is no more. After a interview with former Valve employee
+                Erik Wolpaw, who was in the half life snacks video addressed that unfortunately the Giant pink 
+                cookie that took him weeks to eat is no longer available to snack on in Valve HQ. This comes as a 
+                surprise as the nasty bathroomy trail mix is still available. Wolpaw explained that the giant pink 
+                cookie was removed from the snack room because of the new approach to eating healthy in Valve, as many
+                Valve employees did not want to be eating the same junk all the time.
+            </p>
+        </div>
+        <div>
+            <iframe width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/Lcw4Xlp0AhI" 
+            title="YouTube video player" frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen></iframe>
+        </div>
+    </div>
+
+    <div class="flex flex-col items-center justify-center pt-10 space-y-12 min-h-screen-nonav ">
+        <h1 class="text-2xl text-red-500">TF2 Update</h1>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/YHf7e67T54Y" 
+            title="YouTube video player" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen></iframe>
+        </div>
+        <div>
+            <p class="mx-40 text-center text-white ">
+                - Fixed Your Eternal Reward ragdolls not fading
+            - Fixed Wrap Assassin projectiles colliding with invisible map entities
+               - Fixed ConTracker counting kills twice
+                - Fixed Halloween: Exorcism spell not being triggered by feign deaths
+                - Updated schema drop_type for hats shipped in the last few updates
+                - Updated Spark of Smissmas Unusual effect
+    - Fixed some slight coloring issues
+    - Adjusted lifetime values
+    - Updated The Giftcrafter and Beanie The All-Gnawing to fix material/model issues
+    - Updated the Steam Workshop dialog to support skipping several pages at once
+    - ConVar tf_steam_workshop_page_skip controls the number of pages to skip. Default is 10.
+    - Supports wrapping around the beginning and end of the list
+    - Updated/Added some tournament medals
+    - Updated the localization files
+    - Updated ctf_doublecross_snowy
+    - Fixed missing texture
+    - Fixed a few minor visual issues
+    - Updated pl_breadspace
+    - Fixed some clipping around cliffs and BLU base
+    - Fixed a bug where players could get stuck behind a wall of computers near the final point
+    - Fixed location by BLU spawn where you can build under cliff
+    - Fixed an issue with sound in the final sequence
+    - Fixed decals
+    - Fixed missing Breadcrab animations
+    - Added updated drill model
+    - Added normal maps for the rock cliffs
+    - Added missing decals and logos
+    - Added bread chunks that fly when Big Mama explodes
+    - Optimized areas to reduce lag
+    - Updated backpack and matchmaking images
+            </p>
+        </div>
+    </div>
+    <div class="flex flex-col items-center justify-center pt-10 space-y-12 min-h-screen-nonav ">
+        <h1 class="text-2xl text-red-500">Half Life 2</h1>
+        <div>
+            <img src="https://cdn.akamai.steamstatic.com/steam/apps/220/header.jpg" alt="">
+        </div>
+        <div>
+            <p class="mx-40 text-center text-white ">
+                Half-Life 2 was created using Valve's Source engine, which was developed at the same time. Development lasted five years and cost US$40 million. Valve president Gabe Newell set his team the goal of redefining the first-person shooter genre. They integrated the Havok physics engine, which simulates real-world physics, to reinforce the player's sense of presence and create new gameplay, and developed the characterization, with more detailed character models and realistic animation.
+
+Valve announced Half-Life 2 at E3 2003, with a release date for that September; it was delayed by over a year, triggering a backlash. A year before release, an unfinished version of the game was stolen by a hacker and leaked online; the leak damaged team morale and slowed development.
+
+Half-Life 2 was released on Steam on November 16, 2004, and received acclaim. It won 39 Game of the Year awards and has been cited as one of the best games of all time. By 2011, it had sold 12 million copies. Half-Life 2 was followed by the free extra level Lost Coast (2005) and the episodic sequels Episode One (2006) and Episode Two (2007). In 2020, after canceling several further Half-Life projects, Valve released the prequel Half-Life: Alyx.
+            </p>
+        </div>
+    </div>
+   
 </template>
--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,3 +1,9 @@
 <template>
-    <h1>contact us!</h1>
+<div class="flex flex-col items-center justify-center pt-10 space-y-12 min-h-screen-nonav ">
+    <h1 class="text-3xl font-extrabold text-white">We're here to help!</h1>
+    <p class="mx-40 text-center text-white">
+        If you need customer service or technical support regarding Steam or Valve games, 
+        visit Steam Support at https://help.steampowered.com, where we can most efficiently direct your issue to the attention of the right team.
+    </p>
+</div>
 </template>
